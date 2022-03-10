ad7cd116e09cb5c2978f585b40b3f8de994d51a2
Roge55555 <roge55555@gmail.com>
2022-03-10 23:58:12 +0300

    test fixes

--- src/test/java/com/example/je/services/FilmServiceTest.java
+++ src/test/java/com/example/je/services/FilmServiceTest.java
@@ -1,15 +1,17 @@
 package com.example.je.services;
 
+import com.example.je.Utils;
 import com.example.je.dao.FilmDAO;
+import com.example.je.dao.FullFilmDAO;
 import com.example.je.model.Film;
+import com.example.je.model.FullFilm;
 import com.example.je.model.Page;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
@@ -17,12 +19,17 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class FilmServiceTest {
 
     @Mock
     FilmDAO filmDAO;
+
+    @Mock
+    FullFilmDAO fullFilmDAO;
 
     @Mock
     PageService pageService;
@@ -40,8 +47,8 @@ class FilmServiceTest {
                 .filmLength("09:26")
                 .rating("7.7")
                 .ratingVoteCount(358561L)
-                .posterUrl(null)
-                .posterUrlPreview(null)
+                .posterUrl("https://kinopoiskapiunofficial.tech/images/posters/kp/435.jpg")
+                .posterUrlPreview("https://kinopoiskapiunofficial.tech/images/posters/kp_small/435.jpg")
                 .build();
     }
 
@@ -51,33 +58,66 @@ class FilmServiceTest {
         List<Film> top250;
         int numberOfPages = 13;
 
-        Mockito.doCallRealMethod().when(pageService).addFilms(ArgumentMatchers.any(Page.class), ArgumentMatchers.anyList());
+        doCallRealMethod().when(pageService).addFilms(any(Page.class), anyList());
         top250 = filmService.loadFilms();
 
 
         assertEquals(250, top250.size());
-        Mockito.verify(pageService, Mockito.times(numberOfPages)).addFilms(ArgumentMatchers.any(Page.class), ArgumentMatchers.anyList());
+        verify(pageService, times(numberOfPages)).addFilms(any(Page.class), anyList());
+    }
+
+    @Test
+    @DisplayName("Load full film")
+    void loadFullFilm() {
+        FullFilm fullFilm = null;
+
+        when(filmService.getFilm(anyLong())).thenReturn(newFilm(301L, "Интерстеллар", null));
+        fullFilm = filmService.loadFullFilm(301L);
+
+        assertNotNull(fullFilm);
     }
 
     @Test
     @DisplayName("Save film(s)")
     void addFilms() {
-        List<Film> filmsAdd = new ArrayList<>();
-        filmsAdd.add(newFilm(8L, null, "4400"));
+        try (MockedStatic<Utils> utilities = mockStatic(Utils.class)) {
+            List<Film> filmsAdd = new ArrayList<>();
+            filmsAdd.add(newFilm(8L, "4400", "4400"));
 
-        filmService.saveFilms(filmsAdd);
+            utilities.when(() -> Utils.saveImage(anyString(), anyString())).thenCallRealMethod();
+            filmService.saveFilms(filmsAdd);
 
-        Mockito.verify(filmDAO, Mockito.times(1)).saveAllFilms(ArgumentMatchers.anyList());
+            verify(filmDAO, times(1)).saveAllFilms(anyList());
+        }
+
+    }
+
+    @Test
+    @DisplayName("Save full film")
+    void addFullFilm() {
+        FullFilm fullFilm = new FullFilm();
+
+        filmService.saveFullFilm(fullFilm);
+
+        verify(fullFilmDAO, times(1)).saveFullFilm(any(FullFilm.class));
     }
 
     @Test
     @DisplayName("Get film by id")
     void getFilm() {
-        Mockito.when(filmDAO.getFilm(ArgumentMatchers.anyLong())).thenReturn(newFilm(6L, null, "12 chairs"));
+        when(filmDAO.getFilm(anyLong())).thenReturn(newFilm(6L, null, "12 chairs"));
         Film film = filmService.getFilm(6546L);
 
         assertNotNull(film);
-        Mockito.verify(filmDAO, Mockito.times(1)).getFilm(ArgumentMatchers.anyLong());
+        verify(filmDAO, times(1)).getFilm(anyLong());
+    }
+
+    @Test
+    @DisplayName("Delete film by id")
+    void banFilm() {
+        filmService.banFilm(5485L);
+
+        verify(filmDAO, times(1)).banFilm(anyLong());
     }
 
     @Test
@@ -85,6 +125,6 @@ class FilmServiceTest {
     void deleteFilm() {
         filmService.deleteFilm(641564L);
 
-        Mockito.verify(filmDAO, Mockito.times(1)).deleteFilm(ArgumentMatchers.anyLong());
+        verify(filmDAO, times(1)).deleteFilm(anyLong());
     }
 }
